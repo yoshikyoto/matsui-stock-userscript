5adeae1cef24037645926fb2e6a9cb3405d831a0
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2025-04-01 22:56:39 +0900

    ボードが切り替わったのが検知できるように

--- matsui-v1.0.js
+++ matsui-v1.0.js
@@ -1,22 +1,36 @@
 // ==UserScript==
 // @name         Matsui stock script
-// @namespace    http://tampermonkey.net/
 // @match        https://trade.matsui.co.jp/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=matsui.co.jp
 // @grant        none
+// @grant        GM_log
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // ==/UserScript==
 
 // 必ず Google Chrome の開発者モードを有効にする必要があります
 // https://www.tampermonkey.net/faq.php#Q209
 
-console.log("user script --------------------------------------")
+console.log("user script --------------------------------------");
 
 (function() {
-  'use strict';
+  var previousActiveBoardName = "";
 
-  // Your code here...
   setInterval(function() {
-    console.log("interval --------------------------------------")
+    console.log("interval --------------------------------------");
+    const isPriceBoardOpen = $("div#priceboard-chart").length > 0;
+    console.log("isPriceBoardOpen: " + isPriceBoardOpen);
+    if (isPriceBoardOpen) {
+      let activeBoardName = $("div#priceboard-groups li.active").text();
+      if (previousActiveBoardName !== activeBoardName) {
+        // ボードが切り替わった場合はだけ処理を行う
+        openPriceBoardPage();
+        previousActiveBoardName = activeBoardName;
+      }
+    }
   }, 1000);
+
+  function openPriceBoardPage() {
+    console.log("priceBoardPage の処理");
+
+  }
 })();
